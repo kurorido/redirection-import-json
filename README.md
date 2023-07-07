2467d768dd8fc609fe6388e0c090da17199dea66
kuro roli <kuroridoplayer@gmail.com>
2023-07-07 17:00:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Redirection Import JSON Maker
+
+WordPress Redirection Plugin allows user to import JSON, but make import JSON is requiring huge human work.
+
+This project allow you to input a simple JSON file as source then output a JSON file you can upload directly.
+
+## Usage
+
+prepare `sample.json`
+
+run following command to generate `redirects.json`
+
+```
+node main.js
+```
---
+++ main.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const { URL } = require('url');
+
+const items = JSON.parse(fs.readFileSync('sample.json'))
+
+const redirects = []
+
+for (const item of items) {
+
+    const source = item.source
+    const destination = item.destination
+
+    const urlObject = new URL(source);
+    const domain = urlObject.hostname;
+    const path = urlObject.pathname;
+
+    redirects.push({
+        "url": path,
+        "match_url": path,
+        "match_data": {
+            "source": {
+                "flag_query": "pass",
+                "flag_case": false,
+                "flag_trailing": false,
+                "flag_regex": false
+            }
+        },
+        "action_code": 301,
+        "action_type": "url",
+        "action_data": {
+            "server": domain,
+            "url_from": destination,
+            "url_notfrom": ""
+        },
+        "match_type": "server",
+        "title": "",
+        "hits": 0,
+        "regex": false,
+        "group_id": 1,
+        "position": 0,
+        "last_access": "-",
+        "enabled": true
+    })
+}
+
+fs.writeFileSync('redirects.json', JSON.stringify(redirects, null, 2))
